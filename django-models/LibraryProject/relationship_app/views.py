dc76b1675b104e00004fdc37501d998fa8c6ab95
Ahmed Abasimel <abasimel@Ahmeds-MacBook-Air.local>
2025-11-09 19:55:39 +0100

    Implement user authentication (login, logout, registration) in relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404,redirect
 from django.http import HttpResponse
 from django.views.generic import DetailView
 from .models import Library, Author,Book
 from django.views.generic.detail import DetailView
+
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+
 def list_books(request):
     """
     Function-based view that lists all books in the database
@@ -22,3 +27,30 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['library'] = Library.objects.prefetch_related('books__author').get(pk=self.object.pk)
         return context
+    
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
