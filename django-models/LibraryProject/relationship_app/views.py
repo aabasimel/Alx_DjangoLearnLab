54c0d8d1a96a8b969b72b99c064b6c1782a9a7c5
Ahmed Abasimel <abasimel@Ahmeds-MacBook-Air.local>
2025-11-09 19:35:40 +0100

    Updated

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,10 +4,21 @@ from django.views.generic import DetailView
 from .models import Author,Book,Library
 
 def list_books(request):
-    books = Book.objects.all()
-    return render (request, 'list_books.html',{'books':books})
+    """
+    Function-based view that lists all books in the database
+    """
+    books = Book.objects.all().select_related('author')
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
+    """
+    Class-based view that displays details for a specific library
+    """
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['library'] = Library.objects.prefetch_related('books__author').get(pk=self.object.pk)
+        return context
