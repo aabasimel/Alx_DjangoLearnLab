9a3dd833c8ac7db972c9e53a37d821f03c4d6a9e
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-16 16:45:55 +0100

    Updated

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author, Book, Library, Librarian,UserProfile,User
+from .models import Author, Book, Library, Librarian,UserProfile,CustomUser
 from django.contrib.auth.admin import UserAdmin
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
@@ -19,7 +19,7 @@ class LibrarianAdmin(admin.ModelAdmin):
     list_display = ['name', 'library']
 
 class CustomUserAdmin(UserAdmin):
-    model = User
+    model = CustomUser
     list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth', 'profile_photo']
     list_filter = ['is_staff', 'is_superuser', 'is_active', 'groups','date_of_birth']
     add_fieldsets = (
@@ -39,6 +39,6 @@ class CustomUserAdmin(UserAdmin):
         ),)
     
 
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(UserProfile)
    
