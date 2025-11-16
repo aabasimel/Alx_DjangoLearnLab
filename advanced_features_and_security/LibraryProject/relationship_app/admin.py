1004588d88bae162ecb9e55215dbe800d9673b2d
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-16 16:36:11 +0100

    feat: Define a custom ModelAdmin class that includes configurations for the additional fields that we added to the User model

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Author, Book, Library, Librarian
-
+from .models import Author, Book, Library, Librarian,UserProfile,User
+from django.contrib.auth.admin import UserAdmin
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ['name']
@@ -17,3 +17,28 @@ class LibraryAdmin(admin.ModelAdmin):
 @admin.register(Librarian)
 class LibrarianAdmin(admin.ModelAdmin):
     list_display = ['name', 'library']
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth', 'profile_photo']
+    list_filter = ['is_staff', 'is_superuser', 'is_active', 'groups','date_of_birth']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'first_name',
+                'last_name',
+                'password1',
+                'password2',
+                'date_of_birth',
+                'profile_photo',
+                'is_staff',
+                'is_active'
+            )}
+        ),)
+    
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
+   
