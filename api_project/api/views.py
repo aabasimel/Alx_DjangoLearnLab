084575592c236bfab3c8bae9d39a6ac08287b59a
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-23 19:01:55 +0100

    Added BookViewSet

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,17 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
-
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'author']
+    filterset_fields = ['publication_year']
+    ordering_fields = ['title', 'author', 'publication_year']
+    ordering = ['title']
