9952178ca1b7db46701d50ff2f6767b9a2c6e83f
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-23 17:59:06 +0100

    Updated

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&uoa0g9sn^aoenv*770z%@1jo%+_=cjp7l9gw&fcp0q3+&7q$7"
+SECRET_KEY = "django-insecure-#9kte4@q&tm0!mu9nrrgpc=(3t2mot3n=16a&0kicomn4vq-1&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,4 +122,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-AUTH_USER_MODEL = 'api.User'
