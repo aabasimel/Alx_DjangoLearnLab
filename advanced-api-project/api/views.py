2fa7b2be820f0dd9626ba4b3fa1579843e491cf0
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-30 21:04:28 +0100

    Updated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -9,6 +9,7 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 class AuthorListCreateView(generics.ListCreateAPIView):
     """
     API endpoint that allows authors to be viewed or created.
