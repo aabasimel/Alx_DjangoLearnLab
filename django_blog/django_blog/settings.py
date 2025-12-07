72e9c490409b85b65a86f08065e379ad0343a446
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-12-07 20:24:23 +0100

    Updated

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
+    # Optional: django-taggit for tag management (install with `pip install django-taggit`)
+    "taggit",
 ]
 
 MIDDLEWARE = [
